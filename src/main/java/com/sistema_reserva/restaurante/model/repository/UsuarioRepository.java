10a1d3758fae4163a64009b79956ec8bf5f177b7
SUELITON DE OLIVEIRA (X01142591190) <x01142591190@tcu.gov.br>
2024-10-01 17:40:50 -0300

    end point para salvar usuario

---
+++ src/main/java/com/sistema_reserva/restaurante/model/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.sistema_reserva.restaurante.model.repository;
+
+import com.sistema_reserva.restaurante.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
